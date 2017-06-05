e9a4ada9c32bb90ce7e475806e2de4e327d4ec27
Travis Cox <travis.cox@unimelb.edu.au>
2017-06-05 10:43:43 +1000

    Menu container no longer blocks interaction with map

--- Blood Map Webpage/js/menu.js
+++ Blood Map Webpage/js/menu.js
@@ -1,11 +1,11 @@
 function toggle_menu() {
     $("#menu").fadeToggle("fast");
     $("#menu_background").fadeToggle("fast");
+    $("#menu-container").fadeToggle("fast");
     $("#general").show();
     $(".menu_tab").removeClass("active");
     $(".menu_tab").first().addClass("active");
     $(".menu_button").fadeToggle("fast");
-    
 }
 
 function open_tab(trigger, tabName) {
